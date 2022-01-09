a3811918eb01b077a2a8d006d80351a750a1ae6d
mohamed <moh.essa2020@gmail.com>
2022-01-09 16:04:39 +0200

    comments more and less

--- js/file.js
+++ js/file.js
@@ -47,3 +47,18 @@ var swiper2 = new Swiper('.most-viewed .swiper-container', {
     },
 
   });
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
